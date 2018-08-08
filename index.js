c95430a183ef0d4ecb576343047c12d602d55fd9
Ann Siebecker <asiebecker@hotmail.com>
2018-08-08 23:49:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 /* Enter the code to remove the main node element under this comment */
-
+main.remove();
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+var h1 = document.createElement('h1');
+h1.id = 'victory';
+h1.innerHTML = 'Ann is the champion!'
+
+document.body.appendChild(h1);
